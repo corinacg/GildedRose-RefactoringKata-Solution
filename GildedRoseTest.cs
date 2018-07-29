fede8b85c8691e5e10147d38ce33f89e28a265e2
Corina Ciocanea <corina.ciocanea@gmail.com>
2018-07-29 18:12:05 +0300

    add unit tests

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -7,12 +7,84 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void UpdateQuality_OneItemNameFoo_ItemNameDoesntChange()
+        public void UpdateQuality_OneItemNameFoo_ItemNameDoesNotChange()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void UpdateQuality_OneRegularItem_SellInDecreases()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items[0].SellIn);
+        }
+
+        [Test]
+        public void UpdateQuality_OneRegularItem_QualityDecreases()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_OneRegularItemWithQuality0_QualityDoesNotBecomeNegative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_OneAgedBrieItem_QualityIncreases()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(11, Items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_OneAgedBrieItemWithQuality50_QualityDoesNotExceed50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_OneAgedBrieItem_SellInDecreases()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].SellIn);
+        }
+
+        [Test]
+        public void UpdateQuality_OneAgedBrieItemWithSellByDatePasses_QualityIncreasesByTwo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(12, Items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_OneAgedBrieItemWithQuality49AndSellByDatePasses_QualityDoesNotExceed50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
         }
     }
 }

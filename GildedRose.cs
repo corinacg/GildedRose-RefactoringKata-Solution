bb4e6688ef1c05a025fa3752f34337157d476a9d
Corina Ciocanea <corina.ciocanea@gmail.com>
2018-07-29 19:26:01 +0300

    remove all update logic from  GildedRose.UpdateQuality and refactor

--- GildedRose.cs
+++ GildedRose.cs
@@ -13,44 +13,9 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            var itemsNotUpdatable = new List<Item>();
-            var updatableItems = new List<UpdatableItem>();
-            foreach (var item in Items)
-            {
-                var updatableItem = UpdatableItemFactory.CreateFromItem(item);
-                if (updatableItem != null)
-                {
-                    updatableItems.Add(updatableItem);
-                }
-                else
-                {
-                    itemsNotUpdatable.Add(item);
-                }
-            }
-
-            foreach (var item in updatableItems)
+            foreach(var item in Items.Select(i=> UpdatableItemFactory.CreateFromItem(i)))
             {
                 item.Update();
-            }
-
-            for (var i = 0; i < itemsNotUpdatable.Count; i++)
-            {
-
-                if (itemsNotUpdatable[i].Quality > 0)
-                {
-                    itemsNotUpdatable[i].Quality = itemsNotUpdatable[i].Quality - 1;
-                }
-
-                itemsNotUpdatable[i].SellIn = itemsNotUpdatable[i].SellIn - 1;
-
-                if (itemsNotUpdatable[i].SellIn < 0)
-                {
-                    if (itemsNotUpdatable[i].Quality > 0)
-                    {
-                        itemsNotUpdatable[i].Quality = itemsNotUpdatable[i].Quality - 1;
-                    }
-
-                }
             }
         }
     }
